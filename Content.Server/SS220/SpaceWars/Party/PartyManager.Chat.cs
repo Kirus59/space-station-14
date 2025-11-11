e910c84bcc6d76359e256d5cbc331a3322a81fcc
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-11-11 20:46:00 +0300

    fix PartyManager.Chat.cs

--- Content.Server/SS220/SpaceWars/Party/PartyManager.Chat.cs
+++ Content.Server/SS220/SpaceWars/Party/PartyManager.Chat.cs
@@ -1,7 +1,5 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
-using Content.Shared.Chat;
 using Content.Shared.SS220.SpaceWars.Party;
-using JetBrains.FormatRipper.Elf;
 using Robust.Shared.Player;
 using Robust.Shared.Utility;
 
